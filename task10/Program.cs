f315f503fde295c59ce83bccba865b99557f7e8a
Daria Belova <ruabld@mail.ru>
2022-11-27 20:49:49 +0300

    достаем среднюю цифру поправила

--- task10/Program.cs
+++ task10/Program.cs
@@ -1,9 +1,6 @@
 ﻿Console.WriteLine("Введите трехзначное число");
 int a = Convert.ToInt32(Console.ReadLine());
-if (99<a & a<1000){
-    int fist = a/10;
-    int sec = fist%10;
-    Console.WriteLine(sec);
-}
+if (99<a & a<1000)
+    Console.WriteLine(a/10%10);
 else
 Console.WriteLine("Ведите именно трехзначное число");
