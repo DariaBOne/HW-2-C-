864d84dbaf47d3d6bd7a92396613b7a2e6d36458
Daria Belova <ruabld@mail.ru>
2022-11-28 10:31:02 +0300

    третья цифра цисла

---
+++ task13/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+int digit = 0;
+if (a<=99)
+Console.WriteLine("третьей цифры нет");
+else {
+while (a>=1000)
+{
+    digit = a/10;
+    a = a/10;
+}
+digit = a%10;
+Console.WriteLine(digit);
+}
